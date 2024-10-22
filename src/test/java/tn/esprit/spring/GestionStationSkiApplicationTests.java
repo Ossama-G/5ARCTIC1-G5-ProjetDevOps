33fd7e74507890f0c7fe5746a79dfca539e0a945
Oussama Gammoudi <Oussama.gammoudi@esprit.tn>
2024-10-22 20:33:04 +0100

    fix(tests): add MockBean for ModelMapper in GestionStationSkiApplicationTests to fix ApplicationContext loading issue

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,11 +1,15 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class GestionStationSkiApplicationTests {
 
+	@MockBean
+	private ModelMapper modelMapper;
 	@Test
 	void contextLoads() {
 		// Add your test implementation here
