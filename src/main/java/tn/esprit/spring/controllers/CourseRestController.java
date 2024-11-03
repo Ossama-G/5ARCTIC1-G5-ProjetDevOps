1218b98939710cc5a2077bdcb2babf5487dac359
Ahmed Belhaj Mohamed <ahmed.belhajmohamed@esprit.tn>
2024-11-03 21:14:05 +0100

    fixed security code issues and test error

--- src/main/java/tn/esprit/spring/controllers/CourseRestController.java
+++ src/main/java/tn/esprit/spring/controllers/CourseRestController.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.dto.CourseDTO;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.TypeCourse;
-import tn.esprit.spring.entities.Support;
 import tn.esprit.spring.services.ICourseServices;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Tag(name = "\uD83D\uDCDA Course Management")
+@Tag(name = "\uD83C\uDFC2 Course Management")
 @RestController
 @RequestMapping("/course")
 @RequiredArgsConstructor
@@ -23,15 +22,22 @@ public class CourseRestController {
 
     @Operation(description = "Add Course")
     @PostMapping("/add")
-    public CourseDTO addCourse(@RequestBody CourseDTO courseDTO){
+    public CourseDTO addCourse(@RequestBody CourseDTO courseDTO) {
         Course course = convertToEntity(courseDTO);
         Course savedCourse = courseServices.addCourse(course);
         return convertToDTO(savedCourse);
     }
 
+    @Operation(description = "Retrieve Course by Id")
+    @GetMapping("/get/{id-course}")
+    public CourseDTO getById(@PathVariable("id-course") Long numCourse) {
+        Course course = courseServices.retrieveCourse(numCourse);
+        return convertToDTO(course);
+    }
+
     @Operation(description = "Retrieve all Courses")
     @GetMapping("/all")
-    public List<CourseDTO> getAllCourses(){
+    public List<CourseDTO> getAllCourses() {
         return courseServices.retrieveAllCourses().stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
@@ -39,27 +45,19 @@ public class CourseRestController {
 
     @Operation(description = "Update Course")
     @PutMapping("/update")
-    public CourseDTO updateCourse(@RequestBody CourseDTO courseDTO){
+    public CourseDTO updateCourse(@RequestBody CourseDTO courseDTO) {
         Course course = convertToEntity(courseDTO);
         Course updatedCourse = courseServices.updateCourse(course);
         return convertToDTO(updatedCourse);
-    }
-
-    @Operation(description = "Retrieve Course by Id")
-    @GetMapping("/get/{id-course}")
-    public CourseDTO getById(@PathVariable("id-course") Long numCourse){
-        Course course = courseServices.retrieveCourse(numCourse);
-        return convertToDTO(course);
     }
 
     private CourseDTO convertToDTO(Course course) {
         CourseDTO courseDTO = new CourseDTO();
         courseDTO.setNumCourse(course.getNumCourse());
         courseDTO.setLevel(course.getLevel());
-        courseDTO.setTypeCourse(course.getTypeCourse().name());
-        courseDTO.setSupport(course.getSupport().name());
         courseDTO.setPrice(course.getPrice());
         courseDTO.setTimeSlot(course.getTimeSlot());
+        courseDTO.setTypeCourse(course.getTypeCourse() != null ? course.getTypeCourse().name() : null);
         return courseDTO;
     }
 
@@ -67,10 +65,11 @@ public class CourseRestController {
         Course course = new Course();
         course.setNumCourse(courseDTO.getNumCourse());
         course.setLevel(courseDTO.getLevel());
-        course.setTypeCourse(TypeCourse.valueOf(courseDTO.getTypeCourse()));
-        course.setSupport(Support.valueOf(courseDTO.getSupport()));
         course.setPrice(courseDTO.getPrice());
         course.setTimeSlot(courseDTO.getTimeSlot());
+        if (courseDTO.getTypeCourse() != null) {
+            course.setTypeCourse(TypeCourse.valueOf(courseDTO.getTypeCourse()));
+        }
         return course;
     }
 }
